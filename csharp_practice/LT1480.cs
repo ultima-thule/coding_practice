9231cc466fbc08fc8639244bebe9bc28dd43a762
Joanna Grzywna <joanna.grzywna@gmail.com>
2023-02-24 09:59:37 +0100

    Gitignore added

---
+++ csharp_practice/LT1480.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_practice
+{
+    internal class LT1480
+    {
+        public static int[] RunningSum(int[] nums)
+        {
+            for (int i = 1; i < nums.Length; i++)
+            {
+                nums[i] = nums[i] + nums[i - 1];
+            }
+            return nums;
+        }
+    }
+}
--- csharp_practice/Program.cs
+++ csharp_practice/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using csharp_practice;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var result = LT1480.RunningSum(new int[] { 1, 2, 3, 4, 5 });
+
+        Console.WriteLine($"Result: {result}");
+
+    }
+}
